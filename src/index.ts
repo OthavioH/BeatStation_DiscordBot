f82847e50894ce50354a4fd89eb7dff9385ffe79
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2023-04-11 20:46:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
-import express, { Request } from "express";
 import { Client, GatewayIntentBits } from "discord.js";
 import ListenerController from "./controllers/ListenerController";
 import SettingsController from "./controllers/SettingsController";
 import { prisma } from "./db/prisma";
-import getOAuth2 from "./lib/OAuth2";
 
 dotenv.config();
 
@@ -27,23 +25,6 @@ try {
   });
 
   console.log("Bot started.");
-
-  const app = express();
-
-  app.get("/", async (req: Request, res) => {
-    console.log(req);
-    try {
-      const oauth2 = await getOAuth2(req.query.code as string);
-      settingsController.setToken(oauth2.access_token);
-      return res.send(oauth2).status(200);
-    } catch (error) {
-      return res.sendStatus(400);
-    }
-  });
-
-  app.listen(3000, () => {
-    console.log("App listening on port 3000");
-  });
 } catch (error) {
   console.log(error);
 }
