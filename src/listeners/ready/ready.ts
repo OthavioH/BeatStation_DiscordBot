495dcae4f17cd235a0f820f41b0f4221f0a7037c
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2023-06-27 17:31:10 -0300

    Update ready.ts

--- src/listeners/ready/ready.ts
+++ src/listeners/ready/ready.ts
@@ -4,22 +4,4 @@ import token from "src/config/token";
 
 export default async function ready(c: Client) {
   console.log(`Ready! Logged in as ${c.user?.tag}`);
-  // const commands = commandList.map((command) => command.data.toJSON());
-
-  // console.log(
-  //   `Started refreshing ${commands.length} application (/) commands.`
-  // );
-
-  // const rest = new REST().setToken(token);
-
-  // const data: any = await rest.put(
-  //   Routes.applicationCommands(process.env.CLIENT_ID as string),
-  //   {
-  //     body: commands,
-  //   }
-  // );
-
-  // console.log(
-  //   `Successfully reloaded ${data.length ?? 0} application (/) commands.`
-  // );
 }
