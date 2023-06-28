b9da6edb7b0057d0afd7b358364580e7cbf14a05
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2023-06-28 13:31:42 -0300

    Implemented zod, created routes file and changed the embed builder

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,7 @@ import dotenv from "dotenv";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import client, { settingsController } from "./bot";
-import SettingsController from "./controllers/SettingsController";
-import {
-  Embed,
-  EmbedBuilder,
-  MessageCreateOptions,
-  MessagePayload,
-  TextChannel,
-} from "discord.js";
+import routes from "./routes/index.routes";
 
 dotenv.config();
 
@@ -20,50 +13,7 @@ app.register(cors, {
   origin: "*",
 });
 
-app.get("/", async (request, reply) => {
-  return { hello: "world" };
-});
-
-app.head("/status", async (request, reply) => {
-  return reply.status(200).send("Success");
-});
-
-app.post("/webhook/instagram/newPost", async (request, reply) => {
-  if (client) {
-    const { permalink, imageUrl, caption, mediaType, thumbnailUrl } =
-      request.body as any;
-
-    const channelIdList = await settingsController.getRegisteredChannels();
-
-    channelIdList.forEach(async (channelId) => {
-      const channel = client.channels.cache.get(channelId) as TextChannel;
-
-      if (channel) {
-        const roleId = await settingsController.getRoleId(channel.guild.id);
-        const embed: EmbedBuilder = new EmbedBuilder()
-          .setTitle(caption ?? "Novo post no Instagram")
-          .setDescription(permalink)
-          .setImage(imageUrl)
-          .setColor(8600244)
-          .setFooter({
-            text: "Instagram",
-          })
-          .setAuthor({
-            name: "BeatStation",
-            url: "https://www.instagram.com/beatstation777/",
-          });
-        thumbnailUrl && embed.setThumbnail(thumbnailUrl);
-
-        await channel.send({
-          content: `:60_Instagram: **Post novo no Instagram do BeatStation!!**\n\n<@&${roleId}>`,
-          embeds: [embed],
-        });
-      }
-    });
-
-    return reply.status(200).send("Success");
-  }
-});
+app.register(routes);
 
 app.listen(
   {
---
+++ src/routes/index.routes.ts
@@ -0,0 +1,72 @@
+import { EmbedBuilder } from "@discordjs/builders";
+import {
+  APIMessageActionRowComponent,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  TextChannel,
+} from "discord.js";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+
+import client, { settingsController } from "src/bot";
+
+export default function routes(fastify: FastifyInstance, options: any) {
+  fastify.get("/", async (request, reply) => {
+    return reply.send("This API is the server of the BeatStation discord bot");
+  });
+
+  fastify.head("/status", async (request, reply) => {
+    return reply.status(200).send("Success");
+  });
+
+  fastify.post("/webhook/instagram/newPost", async (request, reply) => {
+    if (client) {
+      const response = z
+        .object({
+          permalink: z.string(),
+          imageUrl: z.string(),
+          caption: z.string(),
+          mediaType: z.string(),
+          thumbnailUrl: z.string(),
+        })
+        .parse(request.body);
+
+      const { permalink, imageUrl, caption, mediaType, thumbnailUrl } =
+        response;
+
+      const channelIdList = await settingsController.getRegisteredChannels();
+
+      channelIdList.forEach(async (channelId) => {
+        const channel = client.channels.cache.get(channelId) as TextChannel;
+
+        if (channel) {
+          const roleId = await settingsController.getRoleId(channel.guild.id);
+          const embed: EmbedBuilder = new EmbedBuilder({
+            title: caption ?? "Novo post no Instagram",
+            color: 8600244,
+            video: mediaType === "VIDEO" ? { url: imageUrl } : undefined,
+            image: mediaType === "IMAGE" ? { url: imageUrl } : undefined,
+            thumbnail: thumbnailUrl ? { url: thumbnailUrl } : undefined,
+            url: permalink,
+            author: {
+              name: "BeatStation",
+              url: "https://www.instagram.com/beatstation777/",
+              icon_url: client.user?.displayAvatarURL(),
+            },
+            footer: {
+              text: "Instagram",
+            },
+          });
+
+          await channel.send({
+            content: `:60_Instagram: **Post novo no Instagram do BeatStation!!**\n\n<@&${roleId}>`,
+            embeds: [embed],
+          });
+        }
+      });
+
+      return reply.status(200).send("Posting on discord...");
+    }
+  });
+}
