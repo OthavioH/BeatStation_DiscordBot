a256226cedef21721d327a12e2b3803cd96e296f
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2023-06-29 23:04:39 -0300

    Update InstagramPostMessage.ts

--- src/shared/messages/InstagramPostMessage.ts
+++ src/shared/messages/InstagramPostMessage.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, MessageCreateOptions, MessagePayload } from "discord.js";
 import InstagramPostEmbed from "../embeds/InstagramPostEmbed";
 import InstagramPostDomain from "../models/domain/InstagramPostDomain";
 import DiscordClient from "src/shared/types/DiscordClient";
@@ -7,10 +7,10 @@ export default function InstagramPostMessage(
   instagramPostInfo: InstagramPostDomain,
   client: DiscordClient,
   roleId: string | null
-) {
+): MessageCreateOptions {
   const embed: EmbedBuilder = InstagramPostEmbed(instagramPostInfo, client);
   return {
-    content: `:60_Instagram: **Post novo no Instagram do BeatStation!!**\n\n${
+    content: `<:60_Instagram:1103736678109167687> **Post novo no Instagram do BeatStation!!**\n\n${
       roleId ? `<@&${roleId}>` : ""
     }`,
     embeds: [embed],
